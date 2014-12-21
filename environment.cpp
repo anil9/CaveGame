fe7f5b13895667ae35beac14da6a12852249984e
aspang1 <anton.spang@gmail.com>
2014-12-21 20:09:51 +0100

    Skelett för environment

---
+++ environment.cpp
@@ -0,0 +1,19 @@
+class environment{
+
+	directions(){
+		//TODO
+	}
+	neighbor(){
+		//TODO
+	}
+	description(){
+		//TODO
+	}
+	enter(){
+		//TODO med parameter env.
+	}
+	go_back(){
+		//TODO
+	}
+
+}
---
+++ environment.h
@@ -0,0 +1,11 @@
+#ifndef ENVIRONMENT_H
+#define ENVIRONMENT_H
+
+class environment{
+
+	directions();
+	neighbor();
+	description();
+	enter();
+	go_back();
+}
