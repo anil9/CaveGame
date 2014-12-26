6ac8a9c2087d85b281d40df70ab3155c2108b0d0
Andreas Nilsson <anil9@kth.se>
2014-12-26 21:01:33 +0100

    La till vissa funktioner. Aktörer har nu liv och attack poäng. Två aktörer kan nu slåss med varandra.

--- actor.cpp
+++ actor.cpp
@@ -19,8 +19,8 @@ void actor::go(std::string direction) {
 	//TODO
 }
 
-void actor::fight(actor target) {
-	//TODO
+void actor::fight(actor& target) {
+	target.remove_health(get_attack_points());
 }
 
 std::string actor::sense() {
@@ -32,7 +32,45 @@ void actor::use_special() {
 	//TODO
 }
 
+void actor::dead() {
+	//TODO
+}
+
+void actor::set_hp(int hp) {
+
+	health = hp;
+}
+
+void actor::set_attack_points(int attack_points) {
+	default_attack_points = attack_points;
+}
+
+int actor::get_attack_points() {
+	return default_attack_points;
+}
+int actor::get_hp() {
+
+	return health;
+}
+
+// Private:
+void actor::remove_health(int dmg){
+	if(health <= dmg) {
+		health = 0;
+		dead();
+	} else {
+		health -= dmg;
+	}
+}
+
+
 int main() {
-	actor test;
+	actor test1;
+	test1.set_attack_points(4);
+	actor test2;
+	test2.set_hp(10);
+	test1.fight(test2);
+	std::cout << test2.get_hp() << std::endl;
+
 	return 0;
 }
--- actor.h
+++ actor.h
@@ -9,12 +9,21 @@ class actor{
 	std::string get_type();
 	void action();
 	void go(std::string);
-	void fight(actor);
+	void fight(actor&);
 	std::string sense();
-	void use_special();
+	void use_special();		// TODO: Should be pure virtual.
+	void dead();
+	int get_hp();
+	void set_hp(int);
+	virtual int get_attack_points();
+	void set_attack_points(int);
+
 
 private:
-std::string type;
+	void remove_health(int);
+	std::string type;
+	int health;
+	int default_attack_points;
 
 
 };
