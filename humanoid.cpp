9f4c722117c8756fd018196d38221f7d560b220c
Andreas Nilsson <anil9@kth.se>
2014-12-29 19:43:54 +0100

    Lagt till humanoid-skalet. Inget körbart pga beroende av bl.a Wearable som inte finns ännu.

---
+++ humanoid.cpp
@@ -0,0 +1,26 @@
+#include "humanoid.h"
+#include <iostream>
+
+using namespace lab3;
+
+Humanoid::Humanoid(std::string name) {
+	this->name = name;
+}
+void Humanoid::change_gear(Wearable& wearable) {
+	//todo
+}
+void Humanoid::talk_to(Actor& actor) {
+	//todo
+}
+void Humanoid::pick_up(Pickup_able& picked_up) {
+	// convert "pick up <sword of awesome>"-string to instance: Weapon awesome_sword
+	// then add to possession vector
+}
+std::string Humanoid::get_name() {
+	return name;
+}
+void Humanoid::drop(Pickup_able& dropping_item){
+	//convert "drop <sword of awesome>"-string to instance: Weapon awesome_sword
+}
+
+
---
+++ humanoid.h
@@ -0,0 +1,24 @@
+#ifndef HUMANOID_H
+#define HUMANOID_H
+#include "actor.h"
+#include <string>
+#include <vector>
+
+namespace lab3{
+
+	class Humanoid: public Actor {
+	public:
+		Humanoid(std::string);
+		void change_gear(Wearable&);
+		void talk_to(Actor&);
+		void pick_up(Pickup_able&);
+		std::string get_name();
+		void drop(Pickup_able&);
+		void set_name(std::string);
+
+	private:
+		std::vector<Pickup_able> possessions;
+		std::string name;
+	};
+}
+#endif
