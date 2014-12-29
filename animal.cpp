e65589c0dfe362c010394a0db8ed6f1fec529baa
Andreas Nilsson <anil9@kth.se>
2014-12-29 20:08:58 +0100

    skal för animal.

---
+++ animal.cpp
@@ -0,0 +1,9 @@
+#include "animal.h"
+
+using namespace lab3;
+Animal::Animal() {
+	//TODO
+}
+Animal::eat() {
+	//TODO
+}
---
+++ animal.h
@@ -0,0 +1,18 @@
+#ifndef ANIMAL_H
+#define ANIMAL_H
+#include "actor.h"
+
+namespace lab3{
+	class Animal: public Actor{
+	public:
+		Animal();
+		void eat();
+
+	private:
+
+
+
+	};
+
+}
+#endif
