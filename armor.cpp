d0f91c71f82bcb684bdd75e453db0581746df2d8
Andreas Nilsson <anil9@kth.se>
2014-12-30 20:07:00 +0100

    la till Armor.

---
+++ armor.cpp
@@ -0,0 +1,26 @@
+#include "armor.h"
+#include <string>
+
+using namespace lab3;
+Armor::Armor(int protection, int max_durability, int weight, std::string description): Wearable(weight, description){
+	this->protection = protection;
+	this->max_durability = max_durability;
+}
+int Armor::get_max_durability(){
+	return max_durability;
+}
+int Armor::get_current_durability(){
+	return current_durability;
+}
+int Armor::get_protection(){
+	if(current_durability > 0) {
+		return protection;
+	} else {
+		return 0;
+	}
+}
+void Armor::set_current_durability(int durability) {
+	if (durability <= max_durability && durability >= 0) {
+		current_durability = durability;
+	}
+}
