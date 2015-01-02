a0a4d275d6deb4cb142e895037ad0d5b883758c4
Andreas Nilsson <anil9@kth.se>
2015-01-02 21:12:50 +0100

    la till locations för actor.

--- actor.h
+++ actor.h
@@ -1,11 +1,12 @@
 #ifndef ACTOR_H
 #define ACTOR_H
+#include "environment.h"
 #include <string>
 
 namespace lab3{
 class Actor{
  public:
-	Actor(); 
+	Actor(Environment&); 
 	std::string get_type();
 	void action();
 	void go(std::string);
@@ -17,6 +18,7 @@ class Actor{
 	void set_hp(int);
 	virtual int get_attack_points();
 	void set_attack_points(int);
+	void set_location(Environment&)
 
 
 private:
@@ -24,6 +26,7 @@ private:
 	std::string type;
 	int health;
 	int default_attack_points;
+	Environment* location;
 
 
 };
