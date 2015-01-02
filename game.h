5e79b44210c2833250bf45b41c4637bb2fdbe1e5
Andreas Nilsson <anil9@kth.se>
2015-01-02 20:21:33 +0100

    Skapade game. Inget kompilerar

---
+++ game.h
@@ -0,0 +1,30 @@
+#ifndef GAME_H
+#define GAME_H
+#include "actor.h"
+#include "environment.h"
+#include "item.h"
+#include <vector>
+#include <string>
+
+namespace lab3{
+
+class Game{
+public:
+Game();
+
+
+
+private:
+	set_real_player(lab3::Humanoid&);
+	run();
+	execute_command(std::string)
+	Humanoid* real_player;
+	bool game_finished = false;
+	bool next_turn = false;
+
+
+	vector<Actor*> actors;
+	vector<Environment*> environments;
+};
+}
+#endif
